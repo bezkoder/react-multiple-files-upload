9fed31f4a985a204d1a14d7b4c2c07c62a4e1a3a
Tien Nguyen Truong <tien.ngtruong@gmail.com>
2020-07-26 16:39:53 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,18 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+import UploadFiles from "./components/upload-files.component";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container" style={{ width: "600px" }}>
+      <div className="my-2">
+        <h3>bezkoder.com</h3>
+        <h4>React upload multiple Files</h4>
+      </div>
+
+      <UploadFiles />
     </div>
   );
 }
